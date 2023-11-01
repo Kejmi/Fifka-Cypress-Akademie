f2254ca9056aaf39a71326f2538b0e1cea9007fa
Kejmi <kejmi.tomek@gmail.com>
2023-11-01 21:58:36 +0100

    lekce xpath

---
+++ cypress/e2e/xpath/xpath_login.cy.js
@@ -0,0 +1,11 @@
+describe('Login tests with XPath selectors', () => {
+    it('Login with Xpath Selectors', () => {
+        cy.visit("https://tredgate.com/pmtool");
+        cy.xpath("//input[@id='username']").type("cy_podzim_2023");
+        cy.xpath("//input[@id='password']").type("CypressPodzim");
+        cy.xpath("//button[@type='submit']").click();
+        cy.xpath("//li[@id='user_dropdown']").click();
+        cy.xpath("//li[@id='logout']").click();
+    });
+    
+});
