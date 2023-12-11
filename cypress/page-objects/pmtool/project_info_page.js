0606b3f06c507cb0386c2d33116891936abe73cf
Kejmi <kejmi.tomek@gmail.com>
2023-12-11 19:43:03 +0100

    ddt lekce

--- cypress/page-objects/pmtool/project_info_page.js
+++ cypress/page-objects/pmtool/project_info_page.js
@@ -1,13 +1,15 @@
 import { HeaderSection } from "./header_section";
 
 export class ProjectInfoPage extends HeaderSection {
-constructor(projectId = "") {
+  constructor(projectId = "") {
     super(`module=items/info&path=21-${projectId}`);
     this.projectTitle = ".portlet-title .caption";
     this.statusDiv = ".check-form-tabs .form-group-157 div";
     this.startDateTd = ".check-form-tabs .form-group-159 td";
     this.addedDateTd = ".check-form-tabs .form-group-154 td";
     this.createdByTd = ".check-form-tabs .form-group-155 td";
+    this.descriptionDiv = ".portlet-body .content_box_content";
+    this.priorityDiv = ".form-group-156 div";
   }
 
   projectTitleHasText(projectTitle) {
@@ -32,6 +34,16 @@ constructor(projectId = "") {
 
   createdByHasText(author) {
     cy.get(this.createdByTd).should("have.text", author);
+    return this;
+  }
+
+  priorityHasText(priority) {
+    cy.get(this.priorityDiv).should("have.text", priority);
+    return this;
+  }
+
+  descriptionHasText(description) {
+    cy.get(this.descriptionDiv).should("have.text", description);
     return this;
   }
 }
